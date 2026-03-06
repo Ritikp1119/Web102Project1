6f67492161647dfeb163d4fe7ba74a8289eb9c32
Ritikp1119 <ritikp1119@gmail.com>
2026-03-05 22:33:41 -0600

    Create card.jsx

---
+++ card.jsx
@@ -0,0 +1,11 @@
+export default function Card({ title, description, link }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        {link && <a href={link} target="_blank">Learn more</a>}
+      </div>
+    </div>
+  );
+}
